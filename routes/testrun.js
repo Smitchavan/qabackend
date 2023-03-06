63c84ca8c0df36f6ed05a984246f90d2c052e59a
Smit Chavan <smit.chavan@slashrtc.com>
2023-03-06 16:46:19 +0530

    updated for vikashs code

--- routes/testrun.js
+++ routes/testrun.js
@@ -53,35 +53,45 @@ router.delete("/deleteruns", async (req, res) => {
   }
 });
 
+// router.post("/updatesteps", async (req, res) => {
+//   let { data, runid } = req.body.data;
+
+//   try {
+
+//     const update = { $set: { "testRun.testcases.$": data } };
+//     const Result = await TestRun.updateOne(
+//       {
+//         _id: runid,
+//         "testRun.testcases._id": data._id,
+//       },
+//       update
+//     );
+
+//     res.send(Result);
+//   } catch (error) {
+//     console.log(error.message);
+//   }
+// });
+
 router.post("/updatesteps", async (req, res) => {
   let { data, runid } = req.body.data;
+  console.log(data);
+  console.log(runid);
+  // try {
 
-  // console.log(data._id);
-  // console.log(data);
-  // console.log(runid);
+  //   const update = { $set: { "testRun.testcases.$": data } };
+  //   const Result = await TestRun.updateOne(
+  //     {
+  //       _id: runid,
+  //       "testRun.testcases._id": data._id,
+  //     },
+  //     update
+  //   );
 
-  try {
-    // let Result = await TestRun.updateOne({
-    //   testRun: { testcases: { _id: data._id } },
-
-    // });
-    // Example testcase _id to update
-
-    // console.log(runid);
-
-    const update = { $set: { "testRun.testcases.$": data } };
-    const Result = await TestRun.updateOne(
-      {
-        _id: runid,
-        "testRun.testcases._id": data._id,
-      },
-      update
-    );
-
-    res.send(Result);
-  } catch (error) {
-    console.log(error.message);
-  }
+  //   res.send(Result);
+  // } catch (error) {
+  //   console.log(error.message);
+  // }
 });
 
 module.exports = router;
